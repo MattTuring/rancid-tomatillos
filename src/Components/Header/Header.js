f82afbd72564e3764d18de23529180acd11f523d
M <m@Ms-MacBook-Pro.local>
2019-12-17 12:11:28 -0700

    Add Header CSS

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import filmStrip from '../../Images/film-strip.png'
 
 const Header = () => {
   return (
+    <>
+    <div className='header-background'>
     <nav>
     <h1>RANCID TOMATILLOS</h1>
     <Link to='/login'>
-    <button className='login-button'>LOG IN</button>
+    <button className='login-button'>LOG&nbsp;IN</button>
     </Link>
     </nav>
+    </div>
+
+    <div className='film-strip' style={{backgroundImage: `url(${filmStrip})`}}>
+    </div>
+    </>
   )
 }
 
---
+++ src/Components/MovieCard/MovieCard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MovieCard = () => {
+
+}
+
+export default MovieCard
