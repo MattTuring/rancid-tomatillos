693ae0b8b9b1fe2b6a6c3c4463b3858f41f32957
Kayla Wood <kaylaewood@gmail.com>
2019-12-18 13:59:54 -0700

    Adjust styling for app

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -5,9 +5,9 @@ const MovieCard = ({title, poster, rating, id}) => {
   console.log(id)
   return (
   <article className='movie-card'>
-    <h2>{title}</h2>
+    <h2>{title.toUpperCase()}</h2>
     <div className='movie-img' style={{backgroundImage: `url(${poster})`}}/>
-    <div className='rating'>RATING: {rating}</div>
+    <div className='rating'>AVERAGE RATING: {rating}</div>
     <Link to={"/movies/" + id}>
       <button>MORE</button>
     </Link>
