1a9f43ba9dcce08e54cca3d5e88ad038fa753787
mjtowarnicky+1@gmail.com <mjtowarnicky+1@gmail.com>
2019-12-17 20:25:49 -0700

    Add iteration 1

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const MovieCard = ({title, poster, rating}) => {
+const MovieCard = ({title, poster, rating, id}) => {
+  console.log(id)
   return (
   <article className='movie-card'>
     <h2>{title}</h2>
     <div className='movie-img' style={{backgroundImage: `url(${poster})`}}/>
-    <div>{rating}</div>
-    <button>MORE</button>
+    <div className='rating'>RATING: {rating}</div>
+    <Link to={"/movies/" + id}>
+      <button>MORE</button>
+    </Link>
   </article>
   )
 }
