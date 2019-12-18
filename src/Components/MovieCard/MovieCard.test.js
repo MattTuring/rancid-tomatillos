4d8857a15e959bceb15bde531c8e0e7af701fc9f
“Foster <fosterataylor@yahoo.com>
2019-12-18 15:37:32 -0700

    Refactor MovieCard test

--- src/Components/MovieCard/MovieCard.test.js
+++ src/Components/MovieCard/MovieCard.test.js
@@ -5,7 +5,9 @@ import MovieCard from './MovieCard';
 describe('MovieCard', () => {
   it('should match the snapshot with all data passed in correctly', () => {
     const wrapper = shallow(<MovieCard
-      title="Foster Movie"
+        title="Foster Movie"
+        averageRating= "55"
+        moreInfo={jest.fn()}
     />);
 
     expect(wrapper).toMatchSnapshot();
