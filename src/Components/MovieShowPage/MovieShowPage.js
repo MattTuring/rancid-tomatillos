3c3cc03aef53e6e04e0002e94fd564ef0ae8e5ac
Kayla Wood <kaylaewood@gmail.com>
2019-12-18 19:55:05 -0700

    Display basic move data on MovieShowPage

--- src/Components/MovieShowPage/MovieShowPage.js
+++ src/Components/MovieShowPage/MovieShowPage.js
@@ -3,14 +3,21 @@ import React from 'react'
 // import { connect } from 'react-redux';
 
 const MovieShowPage = ({ movie }) => {
+  const reorderDate = () => {
+    let dateArray = movie.release_date.split('-');
+    let date = `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`;
+    return date;
+  }
+
   return (
   <section className='movie-show-page'>
     <button>GO BACK</button>
     <div className='movie-info-container'>
-      <div style={{backgroundImage: `url(${movie.poster_path})`}}/>
       <div className='movie-info-text'>
         <h2>{movie.title.toUpperCase()}</h2>
-        <p >AVERAGE RATING: </p>
+        <p>OVERVIEW: {movie.overview}</p>
+        <p>RELEASE DATE: {reorderDate()}</p>
+        <p>AVERAGE RATING: {movie.average_rating}</p>
       </div>
     </div>
   </section>
