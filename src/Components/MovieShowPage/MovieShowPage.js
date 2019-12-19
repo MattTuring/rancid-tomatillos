5a15489bddccc340a6aabcd3f19a678e257bc62b
Kayla Wood <kaylaewood@gmail.com>
2019-12-18 20:10:44 -0700

    Add route back to homepage on go back button

--- src/Components/MovieShowPage/MovieShowPage.js
+++ src/Components/MovieShowPage/MovieShowPage.js
@@ -1,17 +1,18 @@
 import React from 'react'
-// import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const MovieShowPage = ({ movie }) => {
   const reorderDate = () => {
     let dateArray = movie.release_date.split('-');
-    let date = `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`;
+    let date = `${dateArray[1]}-${dateArray[2]}-${dateArray[0]}`;
     return date;
   }
 
   return (
   <section className='movie-show-page'>
-    <button>GO BACK</button>
+    <Link to='/'>
+      <button className='go-back-button'>GO BACK</button>
+    </Link>
     <div className='movie-info-container'>
       <div className='movie-poster' style={{backgroundImage: `url(${movie.poster_path})`}}/>
       <div className='movie-info-text'>
@@ -19,6 +20,7 @@ const MovieShowPage = ({ movie }) => {
         <p>OVERVIEW: {movie.overview}</p>
         <p>RELEASE DATE: {reorderDate()}</p>
         <p>AVERAGE RATING: {movie.average_rating}</p>
+        <button className='rate-button'>RATE THIS MOVIE</button>
       </div>
     </div>
   </section>
