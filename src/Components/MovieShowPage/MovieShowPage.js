0405bb237104c0b666abac7143e74192c2ba8969
Kayla Wood <kaylaewood@gmail.com>
2019-12-18 19:59:23 -0700

    Add movie image and styling to MovieShowPage

--- src/Components/MovieShowPage/MovieShowPage.js
+++ src/Components/MovieShowPage/MovieShowPage.js
@@ -13,6 +13,7 @@ const MovieShowPage = ({ movie }) => {
   <section className='movie-show-page'>
     <button>GO BACK</button>
     <div className='movie-info-container'>
+      <div className='movie-poster' style={{backgroundImage: `url(${movie.poster_path})`}}/>
       <div className='movie-info-text'>
         <h2>{movie.title.toUpperCase()}</h2>
         <p>OVERVIEW: {movie.overview}</p>
