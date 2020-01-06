e251928c925cd61a3abeecfef0d3c8f111a7f6ff
“Foster <fosterataylor@yahoo.com>
2020-01-06 11:35:05 -0700

    Add testing for app mapState and mapDispatch and retrieveMovies method

---
+++ src/Components/App/App.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { App, mapStateToProps, mapDispatchToProps } from './App';
+import { retrieveMovies } from '../../fetchcalls';
+import { addMovies } from '../../actions';
+
+jest.mock('../../fetchcalls.js')
+
+describe('App', () => {
+    beforeEach(() => {
+        retrieveMovies.mockImplementation(() => {
+            return Promise.resolve([{ movie1: 'name' }, { movie2: 'name' }, { movie3: 'name' }])
+        });
+    });
+    
+    it('should invoke retrieveMovies after componentDidMount triggers', () => {
+        shallow(<App />);
+        expect(retrieveMovies).toHaveBeenCalled();
+    });
+    
+    describe('mapsStateToProps', () => {
+        it('should return only movies and loading properties from the store', () => {
+            const mockState = {
+                movies: [],
+                user: {},
+                loading: true
+            };
+            const expected = {
+                movies: [],
+                loading: true
+            };
+            const mappedProps = mapStateToProps(mockState);
+    
+            expect(mappedProps).toEqual(expected);
+        });
+    });
+    
+    describe('mapDispatchToProps', () => {
+        it('calls dispatch with an addMovies action when addMovies is called',
+            () => {
+                const mockDispatch = jest.fn();
+                const actionToDispatch = addMovies([{ sample: 'movie' }]);
+                const mappedProps = mapDispatchToProps(mockDispatch);
+    
+                mappedProps.addMovies([{ sample: 'movie' }]);
+    
+                expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+        });
+    });
+})
